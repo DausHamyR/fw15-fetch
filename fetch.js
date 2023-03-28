c34d7847b391ce3327bd78ccf8b0cde591051550
DausHamyR <daus.amat354@gmail.com>
2023-03-28 13:24:17 +0700

    menampilkan data json

---
+++ fetch.js
@@ -0,0 +1,19 @@
+fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(data => {
+			let tableBody = document.querySelector('table tbody');
+			data.forEach(user => {
+				let row = document.createElement('tr');
+				row.innerHTML = `<td>${user.id}</td>
+                                <td>${user.name}</td>
+                                <td>${user.username}</td>
+                                <td>${user.email}</td>
+                                <td>${user.address.street}</td>
+                                <td>${user.phone}</td>
+                                <td>${user.website}</td>
+                                <td>${user.company.name}</td>`;
+				tableBody.appendChild(row);
+			});
+		})
+		.catch(error => console.error(error));
+        
